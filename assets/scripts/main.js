2d7c751bc66b67093959af91e5efdcd963f85ac2
nessma97 <nessmaelsheikh97@gmail.com>
2022-05-16 23:51:54 +0200

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,128 @@
+let personsContainer = document.querySelector('.persons'); //grid
+var searchPersonsInput = document.querySelector("#search__persons--input");
+let companiesContainer = document.querySelector(".companies");
+let booksContainer = document.querySelector('.books'); 
+let loader = document.querySelector(".loader");
+
+
+function displayPersonsData(persons) {
+persons.forEach((person) => {
+    personsContainer.innerHTML += `
+                <div class="persons__data">
+                    <div class="persons__images">
+                        <img class='perons__images--img' src="${person.image}" alt="A person">
+                    </div>
+                    <div class="persons__info">
+                        <h2><span id='#persons__firstname'>${person.firstname}</span><span>${person.lastname}</span></h2>
+                        <p class="persons__birthday"> ${person.birthday}</p>
+                        <div class="contact__info">
+                            <button class='contact__info--email'>
+                                <a href="mailto: ${person.email}">My Email</a>
+                            </button>
+                            <button class="contact__info--website">
+                                <a href="${person.website}" >Visit my site</a>
+                            </button>
+                        </div>
+                    </div>
+                </div`
+    });    
+}
+
+function getRandomPerson() {
+    loader.classList.remove("hidden");
+    fetch("https://fakerapi.it/api/v1/persons?_quantity=20")
+    .then((response) => response.json())
+    .then((data) => displayPersonsData(data.data))
+    .catch((error) => {
+    console.log("Resquest Failed", error);
+    })
+    .finally ( () => {
+        loader.classList.add("hidden");
+    })
+}
+    // searchPersonsInput.addEventListener('keyup', searchPersons);
+    // function searchPersons() {
+    //     let searchPersonsResult = searchPersonsInput.value.toLowerCase();
+    //     console.log(searchPersonsResult);
+    //     let matchedPerson = companiesContainer.querySelectorAll(".persons--person")
+    //     for (i = 0; i < matchedPerson.length; i++) {
+    //         let nameResult = matchedPerson[i].querySelector('#persons__firstname');
+    //         if (nameResult.innerHTML.toLowerCase().indexOf(searchPersonsResult) > -1) {
+    //             matchedPerson[i].style.display = "intial";
+    //         } else {
+    //             matchedPerson[i].style.display = "none";
+    //         }
+    //     }
+    // }
+getRandomPerson();
+
+function displayCompaniesData(companies) {
+companies.forEach((company) => {
+    companiesContainer.innerHTML += `
+                    <div class="companies__data">
+                        <h3>${company.name}</h3>
+                        <h4>${company.country}</h4>
+                        <div class="company__buttons">
+                            <button class="companies__site">
+                                <a href="${company.website}">Visit our site</a>
+                            </button>
+                            <button class="companies__email">
+                                <a href="mailto: ${company.email}">Email us</a>
+                            </button>
+                            <button class="companies__number">
+                                <a href="tel: ${company.phone}">call us now</a>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="image__box">
+                        <img src="${company.image}" alt="comapny image">
+                    </div>`;
+})
+}
+
+async function getRandomCompanies() {
+await fetch("https://fakerapi.it/api/v1/companies?_quantity=15")
+    .then((response) => response.json())
+    .then((data) => displayCompaniesData(data.data))
+    .catch((error) => {
+    console.log("Resquest Failed", error);
+    });
+}
+getRandomCompanies();
+
+
+function displayBooksData(books) {
+books.forEach((book) => {
+    booksContainer.innerHTML += `
+                <div class="books__data">
+                    <div class="books__images">
+                        <img src="${book.image}" class="books__images--img" alt="books cover">
+                    </div>
+                    <div class="books__info">
+                        <h2 class="book__title">${book.title}</h2>
+                        <h3 class="book__author">${book.author}</h3>
+                        <p class="book__description"> ${book.description} </p>
+                    </div>
+                </div>`;
+});
+}
+
+
+async function getRandomBooks() {
+await fetch("https://fakerapi.it/api/v1/books?_quantity=12")
+        .then((response) => response.json())
+        .then((data) => displayBooksData(data.data))
+        .catch((error) => {
+            console.log("Resquest Failed", error);
+    });
+}
+getRandomBooks();
+
+
+
+// async function getRandomData() {
+//   await getRandomPerson();
+//   await getRandomCompanies();
+//   await getRandomBooks();
+// }
+// getRandomData();
